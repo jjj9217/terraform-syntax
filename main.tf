9a5d68462b8d3b385f76645a86b168f172eb2c84
jjj9217 <albireo9217@gmail.com>
2024-03-07 12:12:43 +0900

    subnet

--- main.tf
+++ main.tf
@@ -22,22 +22,19 @@ resource "aws_vpc" "default" {
 
 # Create a Public Subnet
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.100.0/24"
-
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.100.0/24"
+  availability_zone = "ap-southeast-2a"
   tags = {
     Name = "hangaramit_public_subnet_1"
   }
 }
 
-# resource "aws_subnet" "private_subnet_1" {
-#   vpc_id     = aws_vpc.default.id
-#   cidr_block = "10.0.101.0/24"
-
-#   tags = {
-#     Name = "hangaramit_private_subnet_1"
-#   }
-# }
-data "aws_subnet" "private_subnet_1" {
-  id = "subnet-0d5cfe0e33149def2"
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "ap-southeast-2a"
+  tags = {
+    Name = "hangaramit_private_subnet_1"
+  }
 }
