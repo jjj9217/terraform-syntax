f33fe5f4f6b7e00b6ae613c0b75b71e9789db75a
jjj9217 <albireo9217@gmail.com>
2024-03-07 16:49:23 +0900

    for_작업완료

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-variable "name" {
+variable "names" {
   type    = list(string)
   default = ["스파클", "하나비"]
 }
 
 module "personal_custom_vpc" {
-  for_each = toset(var.name)
+  for_each = toset([for s in var.names : "${s}_test"])
   source   = "./custom_vpc"
   env      = "personal_${each.key}"
 }
